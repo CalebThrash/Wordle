a5cd6bac563542f5c1351bc0916ad7cad8a92a80
Ignacio Secco <isecco@servicetitan.com>
2022-11-17 18:10:44 -0600

    Added Back Button for rules and play scenes

--- src/main/java/com/example/jaxafx_practice/Main.java
+++ src/main/java/com/example/jaxafx_practice/Main.java
@@ -1,121 +1,213 @@
 package com.example.jaxafx_practice;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicReference;
-import static com.example.jaxafx_practice.Resultscene.RestartButton;
 import javafx.scene.paint.*;
-import static com.example.jaxafx_practice.Playscene.guessField;
-
-//import com.example.jaxafx_practice.Playscene.playscence;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.control.*;
+import javafx.scene.Group;
+import javafx.scene.text.TextAlignment;
 
 
 public class Main extends Application {
-    Scene StartScene;
-
-    Scene RulesScene;
+    String userGuess;
     @Override
     public void start(Stage primary) throws IOException {
 
         // ********Start scene************
-
+        //PlayButton.setOnAction((e) -> primaryStage.setScene(PlayScene));??????
+        // Creates Buttons for Start Screen
         Button PlayButton = new Button("Play");
-        PlayButton.setMinWidth(55);
-        PlayButton.setMinHeight(30);
-
-
-        Button RuleButton = new Button("rules");
-        RuleButton.setMinWidth(55);
-        RuleButton.setMinHeight(30);
-
-        VBox layout1 = new VBox(25.0);
+        PlayButton.setMinWidth(70);
+        PlayButton.setMinHeight(40);
+        PlayButton.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 23));
+        VBox layout1 = new VBox(250.0);
+        VBox layout2 = new VBox(25.0);
         layout1.setAlignment(Pos.CENTER);
         Label StartSceneLabel = new Label("Wordle");
-        StartSceneLabel.setMinWidth(55);
-        StartSceneLabel.setMinHeight(30);
+
+
+        StartSceneLabel.setMinWidth(100);
+        StartSceneLabel.setMinHeight(60);
+        StartSceneLabel.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 28));
+        StartSceneLabel.setStyle("-fx-background-color: #381d52;");
+        StartSceneLabel.setTextFill(Color.BEIGE);
         StartSceneLabel.setAlignment(Pos.CENTER);
-        VBox layout2 = new VBox(25.0);
-        layout2.setAlignment(Pos.BOTTOM_CENTER);
-       // layout2.getChildren().addAll(RulesScene, RuleButton);
-        layout2.setStyle("-fx-background-color: #FFFFFF;");
+
+        Button BackButton = new Button("Back");
+        Button BackButton2 = new Button("Back");
+        Button RuleButton = new Button("Rules");
+        BackButton2.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 23));
+        BackButton.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 12));
+        BackButton2.setLayoutX(700);
+        BackButton2.setLayoutY(300);
+        BackButton2.setAlignment(Pos.CENTER);
+
+        RuleButton.setMinWidth(70);
+        RuleButton.setMinHeight(40);
+        RuleButton.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 23));
+        Label RuleSceneLabel = new Label("\n\u2022 You have four chances to guess a randomly selected four-letter word.\n \n\u2022 If you have the right letter in the right spot, it shows up green.\n \n\u2022 A correct letter in the wrong spot shows up yellow.\n \n\u2022 A letter that isn't in the word in any spot shows up gray.");
+        RuleSceneLabel.setTextFill(Color.BEIGE);
+        RuleSceneLabel.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        RuleSceneLabel.setWrapText(true);
+        RuleSceneLabel.setTextAlignment(TextAlignment.JUSTIFY);
 
 
-        StartSceneLabel.setStyle("-fx-background-color: #FFFFFF;");
-        layout1.getChildren().addAll(StartSceneLabel, PlayButton);
+        layout1.getChildren().addAll(StartSceneLabel, PlayButton, RuleButton);
+        layout2.getChildren().addAll(RuleSceneLabel, BackButton2);
         primary.setTitle("Start screen");
-        layout1.setStyle("-fx-background-color:#800000");
-
-        HBox user_interface = new HBox();
-
+        layout1.setStyle("-fx-background-color:#381d52");
+        layout2.setStyle("-fx-background-color:#381d52");
+        HBox user_interface=new HBox();
 
         // Creates Start scene
-        StartScene = new Scene(layout1, 500, 750.0);
+        Scene StartScene = new Scene(layout1, 500, 750.0);
+        Scene RuleScene = new Scene(layout2,500, 750.0);
+
 
         // ********Start scene************
 
+        // ********Play scene************
+        //Creating the word bank
+        //Color myColor= Color.rgb(128,0,0); <-never use????
+        String myword="book"; //<-- the word the user has to guess
+        char[] myWordArray=myword.toCharArray();
+
+        Font myfont=new Font(40);
+
+        // For guessing the characters of the words
+        String lowercase_letter_String="abcdefghijklmnopqrstuvwxyz";
+        char[] lowercase_letterArray=lowercase_letter_String.toCharArray();
+        String uppercase_letter_String="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char[] uppercase_letterArray=uppercase_letter_String.toCharArray();
+
+        //////Creates the blanks for word guess/////////
+        GridPane gp=new GridPane();
+        Label[] labels=new Label[16];
+
+        for(int i=0;i<16;i++)
+        {
+            labels[i]=new Label("_");
+            labels[i].setTextFill(Color.rgb(255,255,255));
+            labels[i].setFont(myfont);
+        }
+        int l=0;
+        for (int j=0; j<4;j++)
+            for (int k=0;k<4;k++)
+            {
+                gp.add(labels[l],j,k);
+                l++;
+            }
+        gp.setVgap(15);
+        gp.setHgap(40);
+        //////Creates the blanks for word guess/////////
+
+        //The Interface For player
+        Label guessLabel=new Label("Guess:");
+        guessLabel.setTextFill(Color.rgb(255,255,255));
+        guessLabel.setMaxHeight(2);
+
+        TextField guessField=new TextField();
+
+        Button enterButton=new Button("Enter");
+        enterButton.setTextFill(Color.rgb(255,255,255));
+        enterButton.setStyle("-fx-background-color: #800000; ");
+
+        HBox user_interface_gs=new HBox();
 
 
-        Playscene playscene = new Playscene();
 
 
-        //Create the word to be guessed
-        Choose_word Choose_word = new Choose_word();
-        AtomicReference<String> wordToGuess = new AtomicReference<>(Choose_word.getWord());
+        user_interface_gs.getChildren().addAll(guessLabel,guessField, enterButton, BackButton);
+        user_interface_gs.setSpacing(15);
+        user_interface_gs.setLayoutY(715);
 
+        gp.setLayoutX(150);
+        gp.setLayoutY(0);
+        gp.setAlignment(Pos.CENTER);
+        gp.setVgap(50);
 
+        Group root_ss=new Group(gp, user_interface_gs);
 
-
+        //Play Scene is created
+        Scene PlayScene=new Scene(root_ss, 500, 750,Color.rgb(56,29,82));
+        // ********Play scene************
 
         //Button actions
         PlayButton.setOnAction((e) -> {
-            primary.setScene(playscene.playscence());
-
+            primary.setScene(PlayScene);
             primary.setTitle("Game Screen");
         });
-        primary.setScene(StartScene);
-        primary.show();
-        playscene.enterButton.setOnAction((e) -> {
-            String userWord = playscene.guessField.getText();
-            if (userWord.length() == 4) {
-                playscene.displayWord(userWord.toLowerCase(), wordToGuess.get());
-                playscene.count += 1;
-            }
-            if (Playscene.count ==4){
-                primary.setScene(Resultscene.loser(wordToGuess.get()));
-            }
+
+        RuleButton.setOnAction((e) -> {
+            primary.setScene(RuleScene);
+            primary.setTitle("Rule Screen");
         });
 
-        //Button actions
-        RestartButton.setOnAction((e) -> {
-            Playscene.count =0;
-            wordToGuess.set(Choose_word.getWord());
-            primary.setScene(playscene.playscence());
+        BackButton.setOnAction((e) -> {
+            primary.setScene(StartScene);
             primary.setTitle("Game Screen");
         });
-        primary.setScene(StartScene);
-        primary.show();
-        playscene.enterButton.setOnAction((e) -> {
-            String userWord = playscene.guessField.getText();
-            if (userWord.length() == 4) {
-                playscene.displayWord(userWord.toLowerCase(), wordToGuess.get());
-                playscene.count += 1;
-            }
-            if (Playscene.count ==4){
-                primary.setScene(Resultscene.loser(wordToGuess.get()));
-            }
+
+        BackButton2.setOnAction((e) -> {
+            primary.setScene(StartScene);
+            primary.setTitle("Game Screen");
         });
+        // input user guess on the screen
+
+        enterButton.setOnAction(e ->{
+
+            String userWord1=guessField.getText();
+            userGuess=userWord1;
+
+                       /* for ( int k=0;k<4;k++) {
+                            int number = k;
+
+                            char[] userWord1array = userWord1.toCharArray();
+                            if (userWord1array[number] == myWordArray[number]) {
+                                //do somthing with color
+                            }
+                            else if (userWord1array[number+1] == myWordArray[number+1]){
+                                //do something with color
+                            }
+                            else if (userWord1array[number+2] == myWordArray[number+2]){
+                                //do something with color
+                            }
+                            else if (userWord1array[number+3] == myWordArray[number+3]){
+                                //do something with color
+                            }
+
+                            String converter = String.valueOf(userWord1array[number]);
+                            labels[number].setText(converter);
+                            converter = String.valueOf(userWord1array[number+1]);
+                            labels[number+4].setText(converter);
+                            converter = String.valueOf(userWord1array[number+2]);
+                            labels[number+8].setText(converter);
+                            converter = String.valueOf(userWord1array[number+3]);
+                            labels[number+12].setText(converter);
+                        }*/
+
+        });
+
 
         primary.setScene(StartScene);
         primary.show();
+
     }
+
 
     public static void main() {
         launch();
-    }}
+    }
+}
 
