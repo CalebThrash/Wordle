0f10ee49a0698e604190f02a1ae85fad32ef5401
Ignacio Secco <isecco@servicetitan.com>
2022-11-17 18:36:08 -0600

    Added Back Button for rules and play scenes

--- src/main/java/com/example/jaxafx_practice/Main.java
+++ src/main/java/com/example/jaxafx_practice/Main.java
@@ -10,6 +10,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
+
 import javafx.scene.paint.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -75,80 +77,13 @@ public class Main extends Application {
         Scene RuleScene = new Scene(layout2,500, 750.0);
 
         Playscene playscene=new Playscene();
-        Choose_word wordToGuess=new Choose_word();
+        Choose_word Choose_word=new Choose_word();
+        AtomicReference<String> wordToGuess =new AtomicReference<>(Choose_word.getWord());
 
 
         // ********Start scene************
 
         // ********Play scene************
-        //Creating the word bank
-        //Color myColor= Color.rgb(128,0,0); <-never use????
-        /*String myword="book"; //<-- the word the user has to guess
-        char[] myWordArray=myword.toCharArray();
-
-        Font myfont=new Font(40);
-
-        // For guessing the characters of the words
-        String lowercase_letter_String="abcdefghijklmnopqrstuvwxyz";
-        char[] lowercase_letterArray=lowercase_letter_String.toCharArray();
-        String uppercase_letter_String="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        char[] uppercase_letterArray=uppercase_letter_String.toCharArray();
-
-        //////Creates the blanks for word guess/////////
-        GridPane gp=new GridPane();
-        Label[] labels=new Label[16];
-
-        for(int i=0;i<16;i++)
-        {
-            labels[i]=new Label("_");
-            labels[i].setTextFill(Color.rgb(255,255,255));
-            labels[i].setFont(myfont);
-        }
-        int l=0;
-        for (int j=0; j<4;j++)
-            for (int k=0;k<4;k++)
-            {
-                gp.add(labels[l],j,k);
-                l++;
-            }
-        gp.setVgap(15);
-        gp.setHgap(40);
-        //////Creates the blanks for word guess/////////
-
-        //The Interface For player
-        Label guessLabel=new Label("Guess:");
-        guessLabel.setTextFill(Color.rgb(255,255,255));
-        guessLabel.setMaxHeight(2);
-
-        TextField guessField=new TextField();
-
-        Button enterButton=new Button("Enter");
-        enterButton.setTextFill(Color.rgb(255,255,255));
-        enterButton.setStyle("-fx-background-color: #800000; ");
-
-        HBox user_interface_gs=new HBox();
-
-
-
-
-        user_interface_gs.getChildren().addAll(guessLabel,guessField, enterButton, BackButton);
-        user_interface_gs.setSpacing(15);
-        user_interface_gs.setLayoutY(715);
-
-        gp.setLayoutX(150);
-        gp.setLayoutY(0);
-        gp.setAlignment(Pos.CENTER);
-        gp.setVgap(50);
-
-        Group root_ss=new Group(gp, user_interface_gs);
-
-        //Play Scene is created
-        Scene PlayScene=new Scene(root_ss, 500, 750,Color.rgb(56,29,82));
-        // ********Play scene************
-
-         */
-
-        //Button actions
         PlayButton.setOnAction((e) -> {
             primary.setScene(playscene.playscence());
             primary.setTitle("Game Screen");
@@ -175,11 +110,11 @@ public class Main extends Application {
             String userWord1=playscene.guessField.getText();
             userGuess=userWord1;
             if(userWord1.length()==4) {
-            playscene.displayWord(userWord1.toLowerCase(),wordToGuess.getWord());
+            playscene.displayWord(userWord1.toLowerCase(),wordToGuess.get());
             playscene.count +=1;
             }
             if (playscene.count==4){
-                primary.setScene((Resultscene.loser(wordToGuess.getWord())));
+                primary.setScene((Resultscene.loser(wordToGuess.get())));
             }
 
 
